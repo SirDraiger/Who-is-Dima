ba0ccaeba039a7580f4f4fbccc30bfee2ea324a8
SirDraiger~ <lekht-k@yandex.ru>
2023-08-30 18:25:48 +0300

    mobile version release

--- script.js
+++ script.js
@@ -8,10 +8,10 @@ let song = new Audio();
 button.addEventListener('click', function() {
     console.log('Диман пивосос');
     answer.textContent = 'Диман пивосос'
-    fon.style.backgroundImage = "url(/img/pivo.jpg)";
+    fon.style.backgroundImage = "url(img/pivo.jpg)";
     fon.style.color = "black";
     photo.src = "img/dima.jpg";
-    photo.style.height = "700px";
+    photo.style.width = "95%";
     photo.style.border = "solid rgb(238, 44, 77) 5px";
     button.style.border = "1px solid black";
     button.style.display = "none";
